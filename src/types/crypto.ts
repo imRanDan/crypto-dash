eba64f76cd35986b7695be3a5ac43d93de871a80
Dan Imran <dan.imran97@gmail.com>
2025-09-13 23:42:10 -0400

    filling out crypto.ts interfaces

--- src/types/crypto.ts
+++ src/types/crypto.ts
@@ -1,7 +1,15 @@
 export interface Coin{
-
+    id: string
+    symbol: string
+    name: string
+    image: string
+    current_price: number
+    market_cap: number
+    market_cap_rank: number
+    price_change_percentage_24h: number
 }
 
 export interface GlobalData {
-    
+    total_market_cap: { used: number }
+    total_volume: { usd: number }
 }
