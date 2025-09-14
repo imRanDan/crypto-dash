348ce15a9c9e74680a2a0dc198fa7e2a47d403e5
Dan Imran <dan.imran97@gmail.com>
2025-09-14 13:01:23 -0400

    create components folder and CoinList component

---
+++ src/components/CoinList.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import { Coin } from "../types/crypto";
+
+const CoinList = () => {
+      const [coins, setCoins] = useState([])
+      const [loading, setLoading] = useState(true)
+
+
+      useEffect(() => {
+        const fetchCoins = async () => {
+            try {
+                
+            } catch (error) {
+                
+            }
+        }
+      })
+}
