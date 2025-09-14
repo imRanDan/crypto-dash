fbd4684baea85783d284b94bddf43839f81fb948
Dan Imran <dan.imran97@gmail.com>
2025-09-14 16:59:15 -0400

    commenting out api test code

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import CoinList from './components/CoinList'
 
 
 function App() {
-  const [coins, setCoins] = useState([])
+  // const [coins, setCoins] = useState([])
 
-  useEffect(() => {
-    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1')
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setCoins(data)
-      })
-  }, [])  
+  // useEffect(() => {
+  //   fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1')
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       console.log(data)
+  //       setCoins(data)
+  //     })
+  // }, [])  
 
   return (
     <div>
